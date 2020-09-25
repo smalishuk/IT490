0efc5a4f7ea990503b6c1f0138cdc99ae226c5ca
mattgclaudio <mc824@njit.edu>
2020-09-25 14:46:03 -0400

    Update tstrb.php

--- tstrb.php
+++ tstrb.php
@@ -21,13 +21,9 @@ $request['username'] = "RABBIT";
 $request['password'] = "YALL";
 $request['message'] = $msg;
 $response = $client->send_request($request);
-//$response = $client->publish($request);
 
-#echo "client received response: ".PHP_EOL;
-#print_r($response);
-#echo "\n\n";
 return $response;
-#echo $argv[0]." END".PHP_EOL;
+
 }
 
 
